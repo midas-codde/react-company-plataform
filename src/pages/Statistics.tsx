fbd77108231e7abbb9a6ecc7e061cf7c75604bae
Midas <richardisraelmagalhaes@gmail.com>
2023-08-24 20:17:48 -0300

    Informações da página Estatisticas

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,10 +1,45 @@
 import SideBar from "../components/SideBar"
+import '../styles/Statistics.scss'
+import iconSearch from '../assets/search.svg'
+import iconNotf from '../assets/notf.svg'
+import iconSettings from '../assets/settings.svg'
+import iconUser from '../images/users/ingrid.png'
 
 const Statistics = () => {
    return (
       <>
          <SideBar />
-         <h1 className="title">Estatisticas</h1>
+         <div className="sttContainer">
+            <div className="sttContainer-top">
+               <div className="topInfo">
+                  <div className="topInfo-left">
+                     <h2>Estatísticas</h2>
+                     <h1>Olá, Ingrid</h1>
+                     <h3>Confira as estatísticas de hoje!</h3>
+                  </div>
+                  <div className="topInfo-right">
+                     <div className="search-container">
+                        <img src={iconSearch} alt="" />
+                        <input type="search" name="search" placeholder="Procurar" id="" />
+                     </div>
+                     <div className="btn-opt">
+                        <img src={iconSettings} alt="" />
+                     </div>
+                     <div className="btn-notf">
+                     <img src={iconNotf} alt="" />
+                     </div>
+                     <div className="icon-user">
+                        <img src={iconUser} alt="" />
+                     </div>
+                     <div className="btn-msg">Mensagem</div>
+                     <div className="btn-sinc">Sincronizar</div>
+                  </div>
+               </div>
+               <div className="topMsg"></div>
+            </div>
+            <div className="sttContainer-center"></div>
+            <div className="sttContainer-bottom"></div>
+         </div>
       </>
    )
 }
