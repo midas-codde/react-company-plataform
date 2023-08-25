3d54fb1c1fe266c27a0bcba57d9ac3475da47062
Midas <richardisraelmagalhaes@gmail.com>
2023-08-25 16:40:43 -0300

    Atualizando props

--- src/components/MsgNotf.tsx
+++ src/components/MsgNotf.tsx
@@ -1,21 +1,21 @@
 import online from '../assets/onlineCircle.svg'
 import '../styles/Statistics.scss'
 
-const MsgNotf = (props) => {
+const MsgNotf = ({img, name, office, msg}) => {
    return (
       <>
          <div className="msgBox">
             <div className="msgBox-top">
-               <div className="msgBox-img"><img src={props.img} alt="" /></div>
+               <div className="msgBox-img"><img src={img} alt="" /></div>
                <p
-                  className="msgBox-name">{props.name}
-                  <p className="msgBox-office">{props.office}</p>
+                  className="msgBox-name">{name}
+                  <p className="msgBox-office">{office}</p>
                </p>
                <img src={online} className="iconOnline" alt="" />
             </div>
             <div className="msgBox-bottom">
                <div className="msgBox-line"></div>
-               <p className="msgBox-msg">{props.msg}</p>
+               <p className="msgBox-msg">{msg}</p>
                <a href="#" className="msgBox-btn"><p>Responder</p></a>
             </div>
          </div>
