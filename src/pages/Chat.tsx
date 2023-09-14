95830a4525aa25cfb77c6ad4af6bcbbe60dc456c
Midas <richardisraelmagalhaes@gmail.com>
2023-09-14 17:43:22 -0300

    Criando a interface

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -2,41 +2,22 @@ import SideBar from "../components/SideBar"
 import Error from '../components/monitorError'
 import "../styles/Chat.scss"
 
-import { useCallback, useState } from "react"
 
 const Chat = () => {
-   const [lista, setLista] = useState<string[]>([]);
-
-   const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
-      if (e.key === 'Enter') {
-         if (e.currentTarget.value.trim().length === 0) return
-
-         const value = e.currentTarget.value;
-
-         e.currentTarget.value = ""
-
-         setLista((oldLista) => {
-            return [...oldLista, value]
-         });
-      }
-   }, []);
-
    return (
       <>
          <Error />
          <SideBar />
-
-         <input
-            className="inputText"
-            onKeyDown={handleInputKeyDown}
-         />
-
-         <ul>
-            {lista.map((value) => {
-               return <li key={value}>{value}</li>
-            })}
-         </ul>
-
+         <div className="chatContainer">
+            <div className="chatContainer-container">
+               <div className="chatContainer-left">
+                  <div className="chatContainer-top">
+                     <div className="userIcon"></div>
+                  </div>
+                     <input type="text" placeholder="Procurar" name="" id="" />
+               </div>
+            </div>
+         </div>
       </>
    )
 }
