0209bd7099651d02dff35df62f65ef24e6c7369e
Midas <richardisraelmagalhaes@gmail.com>
2023-09-14 16:17:23 -0300

    Preparando para criar o chat

--- src/pages/Tables.tsx
+++ src/pages/Tables.tsx
@@ -1,8 +1,11 @@
 import SideBar from "../components/SideBar"
+import Error from "../components/monitorError"
+import "../styles/Tables.scss"
 
 const Tables = () => {
    return (
       <>
+         <Error />
          <SideBar />
          <h1 className="title">Tables</h1>
       </>
